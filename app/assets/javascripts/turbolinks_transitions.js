49c0dc46c0a0a09fbee21f267bbe69c90a68061d
Metaphysiker <s.raess@me.com>
2018-04-06 13:14:15 +0200

    added opacity

--- app/assets/javascripts/turbolinks_transitions.js
+++ app/assets/javascripts/turbolinks_transitions.js
@@ -1,18 +1,68 @@
 $( document ).ready(function() {
 
+    $.MyFunction = function()
+    {
+        console.log("yeeeha");
+    };
+
+
+    $.Transition = function()
+    {
+        console.log("argument");
+        $("#change").addClass("animated bounce");
+    };
+
+    $.TransitionOut = function()
+    {
+        console.log("Out");
+        $(".left").addClass("animated fadeOutLeft");
+    };
+
+
+    $.TransitionIn = function()
+    {
+        console.log("Inx");
+        $(".left").addClass("animated fadeInLeft");
+    };
+
 
     $('a').click(function (e) {
-        e.preventDefault();                   // prevent default anchor behavior
+        e.preventDefault();// prevent default anchor behavior
         var goTo = this.getAttribute("href"); // store anchor href
 
         // do something while timeOut ticks ...
-        console.log( "bounce!" );
-        $("#change").addClass("animated bounce");
+
+        $.TransitionOut();
+
         setTimeout(function(){
             window.location = goTo;
-        },3000);
+        },1000);
     });
 
+
+
+
+
+    (function ( $ ) {
+
+        $.fn.greenify = function( options ) {
+
+            // This is the easiest way to have default options.
+            var settings = $.extend({
+                // These are the defaults.
+                color: "#556b2f",
+                backgroundColor: "white"
+            }, options );
+
+            // Greenify the collection based on the settings variable.
+            return this.css({
+                color: settings.color,
+                backgroundColor: settings.backgroundColor
+            });
+
+        };
+
+    }( jQuery ));
 
 });
 
