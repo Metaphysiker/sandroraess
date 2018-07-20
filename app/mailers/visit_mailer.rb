825b98f31c74173e844ec4127987df2ef27af815
Metaphysiker <s.raess@me.com>
2018-07-20 23:25:32 +0200

    fix mailer

--- app/mailers/visit_mailer.rb
+++ app/mailers/visit_mailer.rb
@@ -1,7 +1,7 @@
 class VisitMailer < ApplicationMailer
   default from: "philotester5@gmail.com"
 
-  def visit_email(params)
+  def visit_email(params, current_visit)
     @params = params
     @currentvisit = current_visit
   mail(to: 's.raess@me.com', subject: 'Visit!')
